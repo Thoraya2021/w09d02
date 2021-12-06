796843ffba8081021105bd3aaebdbd7fb7757b36
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-12-06 16:35:58 +0300

    signup

---
+++ task/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState ,useEffect} from "react";
+import axios from "axios";
+import './App.css';
+
+
+
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //const [role, setRole] = useState("61aca7bb67df5f2e24b890a9");
+  //const [token,setToken]= useState("");
+
+
+/*
+  useEffect(() => {
+    //  const token = localStorage.getItem("token");
+     // setToken(token);
+  }, [])
+*/
+
+  const register = async () => {
+    try{
+    const result=await axios.post(`${BASE_URL}/signup`,
+    {
+      email:email,
+      password:password,
+     // role:role
+    });
+    console.log(result)
+    }
+    catch(error)
+    {
+      console.log(console.error)
+    }
+    
+      };
+  
+
+  return (
+    <div className="register">
+      <input
+        placeholder="email"
+        type="text"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        placeholder="password"
+        type="text"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={register}>sign-up</button>
+      <br />
+
+    </div>
+  );
+}
+
+export default App;
